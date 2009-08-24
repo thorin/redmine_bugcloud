f741da5f26af7c32c84856855bd14b28056c9cac
unknown <moises.trovo@.CPACONSULTING.COM.BR>
2009-08-24 14:21:31 -0300

    First attempt to provide some visual information based on the usage of the issue being shown

--- app/controllers/bugcloud_controller.rb
+++ app/controllers/bugcloud_controller.rb
@@ -1,8 +1,14 @@
 class BugcloudController < ApplicationController
   unloadable
-
+  
   def index
     @project = Project.find params[:id]
-    @issues = @project.issues
+    @issues_freq = Hash.new
+    @project.issues.each do |el|
+    	@issues_freq[el] = 	el.journals.length * 4 + 
+                          el.relations.length * 2 + 
+                          el.priority.position 
+    end
+    
   end
 end
